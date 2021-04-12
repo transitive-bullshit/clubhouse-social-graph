2e5208b719abae3af9947af0f6f207030ab0edf9
Travis Fischer <fisch0920@gmail.com>
2021-04-12 03:15:43 -0400

    ⚫

--- components/SocialGraphVisualization/force-graph.tsx
+++ components/SocialGraphVisualization/force-graph.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { ForceGraph2D } from 'react-force-graph'
+import ForceGraph2D from 'react-force-graph-2d'
 
 export default function ForceGraph({ fgRef, ...props }) {
   return <ForceGraph2D {...props} ref={fgRef} />
